0263dcfb9b7bf3e3ee507a03eb521fc3d240c11b
rashancoder <74274755+rashancoder@users.noreply.github.com>
2021-07-28 17:24:00 -0700

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,34 @@
+class SlingShot{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 0.04,
+            length: 30
+        }
+        this.pointB = pointB
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    display(){
+        if(this.sling.bodyA){
+            var pointA = this.sling.bodyA.position;
+            var pointB = this.pointB;
+            push();
+            stroke(color);
+            strokeWeight(4);
+            line(pointA.x, pointA.y, pointB.x, pointB.y);
+            pop()
+        }
+    }
+
+    fly(){
+        this.sling.bodyA = null;
+    }
+    attach(body){
+        this.sling.bodyA = body;
+    }
+    
+    
+}
