8ddcf6a3e20b6f8d79b20e116adcf9890d158067
sadhanananna <32163265+sadhanananna@users.noreply.github.com>
2021-12-23 03:56:54 +0530

    Add files via upload

--- Ground.js
+++ Ground.js
@@ -1,17 +1,17 @@
-class Ground {
-    constructor(x,y,width,height) {
-      var options = {
-          isStatic: true
-      }
-      this.body = Bodies.rectangle(x,y,width,height,options);
-      this.width = width;
-      this.height = height;
-      World.add(world, this.body);
-    }
-    display(){
-      var pos =this.body.position;
+class Ground{
+
+  constructor(){
+      var ground_options={
+          isStatic : true
+        }
+      
+        this.ground = Bodies.rectangle(450,390,900,20,ground_options)
+        World.add(world,this.ground);
+  }
+  display(){
+      noStroke();
+      fill(188,67,67);
       rectMode(CENTER);
-      fill("brown");
-      rect(pos.x, pos.y, this.width, this.height);
-    }
-  };
+      rect(this.ground.position.x,this.ground.position.y,900,20);
+  }
+}
--- Slingshot.js
+++ Slingshot.js
@@ -1,34 +1,32 @@
-class SlingShot{
+class Slingshot{
     constructor(bodyA, pointB){
         var options = {
             bodyA: bodyA,
             pointB: pointB,
             stiffness: 0.04,
-            length: 30
+            length: 1
         }
-        this.pointB = pointB
         this.sling = Constraint.create(options);
+        this.pointB=pointB;
         World.add(world, this.sling);
-    }
 
-    display(){
-        if(this.sling.bodyA){
-            var pointA = this.sling.bodyA.position;
-            var pointB = this.pointB;
-            push();
-            stroke(color);
-            strokeWeight(4);
-            line(pointA.x, pointA.y, pointB.x, pointB.y);
-            pop()
-        }
-    }
-
-    fly(){
-        this.sling.bodyA = null;
     }
     attach(body){
         this.sling.bodyA = body;
     }
-    
+    fly(){
+        this.sling.bodyA =null;
+        
+    }
+    display(){
+        if(this.sling.bodyA){
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB;
+        
+        strokeWeight(4);
+        stroke("turquoise");
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+        }
     
 }
